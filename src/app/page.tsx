36c0bd9125405b63fb76e29e1ed8ed663cbe4d7a
elyonxxx <elyonfrancine.bana-23@cpu.edu.ph>
2025-11-22 23:41:50 +0800

    [add] skill section changed the design a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Navigation from "../components/navigation"
 import HeroSection from "@/src/components/hero-section"
 import AboutSection from "../components/about-section"
+import SkillsSection from "../components/skill-section"
 export default function Home() {
 
   return (
@@ -10,7 +11,8 @@ export default function Home() {
       <Navigation />
       <HeroSection />
       <AboutSection/>
-
+      <SkillsSection/>
     </main>
   )
+
 }
